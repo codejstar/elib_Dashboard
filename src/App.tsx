2dcccb9989138d5f19f5d3c48ee4caecf54b64b1
code jstar <codejstar@gmail.com>
2024-06-07 17:14:39 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
       <h1>Welcome to elib dashboard</h1>
+       <Button variant={'default'}>Click here</Button>
     </>
   )
 }
