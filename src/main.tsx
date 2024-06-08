51e612e82b1e7d58acfa1ed39c3c4d6ec3a23027
code jstar <codejstar@gmail.com>
2024-06-08 19:18:43 +0530

    add TankStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  QueryClientProvider,
+  QueryClient
+} from '@tanstack/react-query'
 // import App from './App.tsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router.tsx'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}/>
+     </QueryClientProvider>
   </React.StrictMode>,
 )
