990eeade8388b582ba06ea7d75af6bda9ddd2f08
code jstar <codejstar@gmail.com>
2024-06-12 23:11:49 +0530

    add active link in dashboard navbar

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import {Link, Navigate, Outlet} from "react-router-dom"
+import {Link, NavLink, Navigate, Outlet} from "react-router-dom"
 import {
   Bell,
   CircleUser,
@@ -64,20 +64,26 @@ const {token, setToken} = useTokenStore(state => state);
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
+              <NavLink
                 to="/dashboard/home"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                className={({isActive}) => {
+                  return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${isActive && 'bg-muted'}`
+    
+                }}
               >
                 <Home className="h-4 w-4" />
                 Home
-              </Link>
-              <Link
+              </NavLink>
+              <NavLink
                 to="/dashboard/books"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                className={({isActive}) => {
+                  return `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${isActive && 'bg-muted'}`
+    
+                }}
               >
                 <Package className="h-4 w-4" />
                 Books{" "}
-              </Link>
+              </NavLink>
             </nav>
           </div>
           <div className="mt-auto p-4">
